c12b80db882ddabc3ee91cfb378032b5027de89e
Frederic Bregier <nomail@nomail.com>
2009-06-25 20:17:09 +0000

    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateR66/trunk@137 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/java/openr66/protocol/localhandler/LocalTransaction.java
+++ src/main/java/openr66/protocol/localhandler/LocalTransaction.java
@@ -3,6 +3,9 @@
  */
 package openr66.protocol.localhandler;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.Channel;
@@ -13,6 +16,8 @@ import org.jboss.netty.channel.group.DefaultChannelGroup;
 import org.jboss.netty.channel.local.DefaultLocalClientChannelFactory;
 import org.jboss.netty.channel.local.DefaultLocalServerChannelFactory;
 import org.jboss.netty.channel.local.LocalAddress;
+import org.jboss.netty.handler.execution.ExecutionHandler;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 /**
  * This class handles Local Transaction connections
@@ -36,17 +41,35 @@ public class LocalTransaction {
 
     private final ChannelGroup localChannelGroup = new DefaultChannelGroup(
             "LocalChannels");
-
+    
+    //FIXME configuration class
+    /**
+     * Number of threads allowed by handlers
+     */
+    public static int SERVER_THREAD = 8;
+    /**
+     * Max global memory limit: default is 4GB
+     */
+    public static long maxGlobalMemory = 0x100000000L;
+    /** 
+     * Memory limitation: 100MB by channel, 1GB global, 200 ms of
+     * timeout
+    */
+    private volatile OrderedMemoryAwareThreadPoolExecutor pipelineExecutor = 
+        new OrderedMemoryAwareThreadPoolExecutor(
+                SERVER_THREAD,
+                maxGlobalMemory / 10,
+                maxGlobalMemory, 200,
+                TimeUnit.MILLISECONDS, Executors.defaultThreadFactory());
+    
     public LocalTransaction() {
-        serverBootstrap.getPipeline().addLast("handler",
-                new LocalServerHandler());
+        serverBootstrap.setPipelineFactory(new LocalServerPipelineFactory());
         serverChannel = serverBootstrap.bind(socketServerAddress);
         localChannelGroup.add(serverChannel);
+        clientBootstrap.setPipelineFactory(new LocalClientPipelineFactory(pipelineExecutor));
     }
 
     public Channel createNewClient() {
-        clientBootstrap.getPipeline().addLast("handler",
-                new LocalClientHandler());
         ChannelFuture channelFuture = clientBootstrap
                 .connect(socketServerAddress);
         channelFuture.awaitUninterruptibly();
@@ -58,13 +81,13 @@ public class LocalTransaction {
     public void clientChannelClose(Channel channel) {
         channel.close();
         channel.getCloseFuture().awaitUninterruptibly();
-
     }
 
     public void closeAll() {
         localChannelGroup.close().awaitUninterruptibly();
         clientBootstrap.releaseExternalResources();
         channelClientFactory.releaseExternalResources();
+        pipelineExecutor.shutdownNow();
         serverBootstrap.releaseExternalResources();
         channelServerFactory.releaseExternalResources();
     }
