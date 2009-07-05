c0e3ef28eb5ed42ac1daf6407ea25594595afdef
Frederic Bregier <nomail@nomail.com>
2009-07-05 12:32:29 +0000

    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateR66/trunk@176 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/java/openr66/protocol/networkhandler/NetworkTransaction.java
+++ src/main/java/openr66/protocol/networkhandler/NetworkTransaction.java
@@ -4,8 +4,11 @@
 package openr66.protocol.networkhandler;
 
 import java.net.SocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 import openr66.protocol.config.Configuration;
 import openr66.protocol.exception.OpenR66ProtocolNetworkException;
@@ -14,6 +17,7 @@ import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFactory;
 import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.group.ChannelGroup;
 import org.jboss.netty.channel.group.DefaultChannelGroup;
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
@@ -24,12 +28,22 @@ import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
  * @author frederic bregier
  */
 public class NetworkTransaction {
+    static class NetworkChannel {
+        AtomicInteger count = new AtomicInteger(1);
+        Channel channel;
+        public NetworkChannel(Channel channel) {
+            this.channel = channel;
+        }
+    }
+    private static ConcurrentHashMap<Integer, NetworkChannel> networkChannelConcurrentHashMap =
+        new ConcurrentHashMap<Integer, NetworkChannel>();
+    private static ReentrantLock lock = new ReentrantLock();
+    
     /**
      * ExecutorService Server Boss
      */
     private final ExecutorService execServerBoss = Executors
             .newCachedThreadPool();
-
     /**
      * ExecutorService Server Worker
      */
@@ -50,13 +64,21 @@ public class NetworkTransaction {
     }
 
     public Channel createNewClient(SocketAddress socketServerAddress) throws OpenR66ProtocolNetworkException {
-        final ChannelFuture channelFuture = clientBootstrap
-                .connect(socketServerAddress);
-        channelFuture.awaitUninterruptibly();
-        if (channelFuture.isSuccess()) {
-            final Channel channel = channelFuture.getChannel();
-            networkChannelGroup.add(channel);
-            return channel;
+        ChannelFuture channelFuture = null;
+        for (int i = 0; i < Configuration.RETRYNB; i++) {
+            channelFuture = clientBootstrap
+                    .connect(socketServerAddress);
+            channelFuture.awaitUninterruptibly();
+            if (channelFuture.isSuccess()) {
+                final Channel channel = channelFuture.getChannel();
+                networkChannelGroup.add(channel);
+                return channel;
+            }
+            try {
+                Thread.sleep(Configuration.RETRYINMS);
+            } catch (InterruptedException e) {
+                throw new OpenR66ProtocolNetworkException("Cannot connect to remote server",e);
+            }
         }
         throw new OpenR66ProtocolNetworkException("Cannot connect to remote server", channelFuture.getCause());
     }
@@ -65,5 +87,35 @@ public class NetworkTransaction {
         networkChannelGroup.close().awaitUninterruptibly();
         clientBootstrap.releaseExternalResources();
         channelClientFactory.releaseExternalResources();
+    }
+    public static void addNetworkChannel(Channel channel) {
+        lock.lock();
+        try {
+            NetworkChannel networkChannel = networkChannelConcurrentHashMap.get(channel.getId());
+            if (networkChannel != null) {
+                networkChannel.count.incrementAndGet();
+            } else {
+                networkChannel = new NetworkChannel(channel);
+                networkChannelConcurrentHashMap.put(channel.getId(), networkChannel);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+    public static void removeNetworkChannel(Channel channel) {
+        lock.lock();
+        try {
+            NetworkChannel networkChannel = networkChannelConcurrentHashMap.get(channel.getId());
+            if (networkChannel != null) {
+                if (networkChannel.count.decrementAndGet() == 0) {
+                    networkChannelConcurrentHashMap.remove(channel.getId());
+                    Channels.close(channel);
+                }
+            } else {
+                Channels.close(channel);
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 }
