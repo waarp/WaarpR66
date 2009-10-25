e0bbfae880d00653d70127deec8f4465e390a363
Frederic Bregier <nomail@nomail.com>
2009-10-25 14:39:32 +0000

    small fix on example directory, no impact on source
    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateR66/trunk@434 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/example/bin/server.sh
+++ src/main/example/bin/server.sh
@@ -1,5 +1,5 @@
 echo Start R66Server
-. /appli/R66/ENVR66
+. /appli/R66/ENV_R66
 nohup ${JAVARUNSERVER} openr66.server.R66Server ${GGHOME}/conf/config`hostname`.xml 0</dev/null 2>&1 >> ${GGHOME}/log/R66Server.log &
 echo $! > ${GGHOME}/log/lastpid
 echo R66Server started
