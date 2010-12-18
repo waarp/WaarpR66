812fb0266ba8cdd038d8e6cf982957813d8240db
Frederic Bregier <nomail@nomail.com>
2010-12-18 17:22:21 +0000

    Update doc
    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateR66/trunk@736 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/java/openr66/database/data/package-info.java
+++ src/main/java/openr66/database/data/package-info.java
@@ -2,7 +2,7 @@
  * Classes implementing Database Data model
  *
  * @apiviz.landmark
- * @apiviz.uses {@link openr66.databaseold}
+ * @apiviz.uses {@link openr66.database}
  */
 package openr66.database.data;
 
