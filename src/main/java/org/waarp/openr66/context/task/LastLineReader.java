33e1bebe286c1121957868145b36007e779ac252
Frederic Bregier <fredbregier@free.fr>
2013-11-12 20:43:33 +0100

    Next findbugs fix

--- src/main/java/org/waarp/openr66/context/task/LastLineReader.java
+++ src/main/java/org/waarp/openr66/context/task/LastLineReader.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PipedInputStream;
 
+import org.waarp.common.utility.WaarpStringUtils;
+
 /**
  * This class is used with external process in order to get the last echo from the stdout of the
  * process.
@@ -37,7 +39,7 @@ class LastLineReader implements Runnable {
 	public String lastLine = null;
 
 	public LastLineReader(PipedInputStream inputStream) {
-		reader = new BufferedReader(new InputStreamReader(inputStream));
+		reader = new BufferedReader(new InputStreamReader(inputStream, WaarpStringUtils.UTF8));
 	}
 
 	public void run() {
