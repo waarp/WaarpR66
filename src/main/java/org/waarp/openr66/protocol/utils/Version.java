c8ef4b697168cb400cdf212ef94c95516a65590c
Bruno Carlin <bruno.carlin@waarp.org>
2019-05-10 11:49:04 +0200

    bump version to 3.0.12

--- src/main/java/org/waarp/openr66/protocol/utils/Version.java
+++ src/main/java/org/waarp/openr66/protocol/utils/Version.java
@@ -3,7 +3,7 @@ package org.waarp.openr66.protocol.utils;
 /** Provides the version information of Waarp OpenR66. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "3.0.11";
+ public static final String ID = "3.0.12";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
